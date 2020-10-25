ede8e1788500ab7e0a1fdb88b1774c17406ac85b
digocorbellini <digocorbellini@gmail.com>
2020-10-25 09:42:25 -0500

    Deleted uncessesary files

--- static/scripts/index.js
+++
@@ -1,3 +0,0 @@
-function getFile(){
-    let file = document.getElementById("fileUploaded");
-}
--- tester.py
+++
@@ -1,5 +0,0 @@
-import re
-
-htmlFile = open("templates/result.html", "w")
-finalString = "{% extends 'base.html' %}\n{% block body %}{% endblock %}"
-htmlFile.write(finalString)

a31c15f7058a628e716e4d6961f46ae2bb6522a3
digocorbellini <digocorbellini@gmail.com>
2020-10-24 22:27:01 -0500

    made styleChecker a method

--- styleChecker.py
+++ styleChecker.py
@@ -1,7 +1,7 @@
 import re
 
 #place all lines from the file into an array
-with open('static/uploads/text.txt') as f:
+with open('text.txt') as f:
     lines = [line.rstrip() for line in f]
 
 problematicLines = {}
@@ -115,86 +115,83 @@ def checkOperatorSpacing(line, index):
             numOfOperators += 1
         lineIndex += 1
 
-''' def styleCheck():   
+def styleCheck():   
     global inClass
     global haveReachedFirstMethod
     global isComment
     global inMethod
     global operatorSpacing
-    global bracketInLine '''
-#go through all of the lines in the code
-index = 0
-for line in lines:
-    #check to see if lines meet 80 character requirement
-    if(len(line) > 80):
-        problematicLines[index] = "Lines can not exceede 80 characters."
+    global bracketInLine
+    #go through all of the lines in the code
+    index = 0
+    for line in lines:
+        #check to see if lines meet 80 character requirement
+        if(len(line) > 80):
+            problematicLines[index] = "Lines can not exceede 80 characters."
 
-    #Check for start of comment
-    if("/*" in line):
-        isComment = True
-
-
-    #If in the class and have not reached first method, check instance variables
-    if(inClass and not haveReachedFirstMethod):
-        instanceVariableProblem(line, index)
-
-    if(not haveReachedFirstMethod and inClass and isMethod(line,index)):
-        haveReachedFirstMethod = True
-        if("{" not in line and "{" in lines[index + 1]):
-            bracketInLine = False
+        #Check for start of comment
+        if("/*" in line):
+            isComment = True
 
 
-    if(haveReachedFirstMethod and isLine(line) and not isComment):
-        #check for break statements
-        if("break" in line):
-            problematicLines[index] = "No break statements allowed."
+        #If in the class and have not reached first method, check instance variables
+        if(inClass and not haveReachedFirstMethod):
+            instanceVariableProblem(line, index)
 
-        
-        if(inMethod):
-            #if in a method, count the number of lines
-            checkMethodLength(line, index)
-            #Check operator spacing
-            if(operatorSpacing == 0):
-                lineIndex = 0
-                for x in line:
-                    if(x in operators):
-                        if(line[lineIndex - 1] == " "):
-                            operatorSpacing += 1
-                        if(line[lineIndex + 1] == " "):
-                            operatorSpacing += 1
-                        break
-                    lineIndex += 1
-            else:
-                checkOperatorSpacing(line, index)
+        if(not haveReachedFirstMethod and inClass and isMethod(line,index)):
+            haveReachedFirstMethod = True
+            if("{" not in line and "{" in lines[index + 1]):
+                bracketInLine = False
 
-        #find the start of a method
-        #Check for method comment and consistency with brackets
-        if(not inMethod and isMethod(line, index)):
-            inMethod = True
-            #check method comment
-            methodCommentCheck(index)
-            #check inconsistency with brackets
-            checkMethodBrackets(line, index)  
-            #append bracket 
-            if("{" in line):
-                bracketStack.append("{")
-        
-        #check magic numbers
-        checkMagicNumbers(line,index)
-                    
-    #Only start checking lines if the class has been reached
-    if("class" in line):
-        inClass = True        
-    #Check to see if it is the end of a comment
-    if("*/" in line):
-        isComment = False
-        
-    index += 1
 
-        #finalHtml = "<h1>Test Header</h1>"
-        #return finalHtml
+        if(haveReachedFirstMethod and isLine(line) and not isComment):
+            #check for break statements
+            if("break" in line):
+                problematicLines[index] = "No break statements allowed."
 
-#styleCheck()
+            
+            if(inMethod):
+                #if in a method, count the number of lines
+                checkMethodLength(line, index)
+                #Check operator spacing
+                if(operatorSpacing == 0):
+                    lineIndex = 0
+                    for x in line:
+                        if(x in operators):
+                            if(line[lineIndex - 1] == " "):
+                                operatorSpacing += 1
+                            if(line[lineIndex + 1] == " "):
+                                operatorSpacing += 1
+                            break
+                        lineIndex += 1
+                else:
+                    checkOperatorSpacing(line, index)
+
+            #find the start of a method
+            #Check for method comment and consistency with brackets
+            if(not inMethod and isMethod(line, index)):
+                inMethod = True
+                #check method comment
+                methodCommentCheck(index)
+                #check inconsistency with brackets
+                checkMethodBrackets(line, index)  
+                #append bracket 
+                if("{" in line):
+                    bracketStack.append("{")
+            
+            #check magic numbers
+            checkMagicNumbers(line,index)
+                        
+        #Only start checking lines if the class has been reached
+        if("class" in line):
+            inClass = True        
+        #Check to see if it is the end of a comment
+        if("*/" in line):
+            isComment = False
+            
+        index += 1
+
+styleCheck()
 print(problematicLines)
 
 
